607c537a9adb8f7617d1b28aa36a548c463263b7
omranali2111 <aljabri1234@hotmail.com>
2023-09-27 09:38:55 +0400

    creating API of library system

---
+++ RestAPI_Library_Management_System/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI_Library_Management_System
+{
+    public class AppDbContextDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=(local);Initial Catalog=EFCoreLibrary; Integrated Security=true; TrustServerCertificate=True");
+        }
+    }
+    
+    
+}
