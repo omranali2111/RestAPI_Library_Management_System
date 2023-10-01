402f1bf0ee549308865afbd02d2c0d2ca5a991c2
omranali2111 <aljabri1234@hotmail.com>
2023-10-01 10:19:17 +0400

    adding functions for HttpGet

--- RestAPI_Library_Management_System/Controllers/BorrowingTransactionController.cs
+++ RestAPI_Library_Management_System/Controllers/BorrowingTransactionController.cs
@@ -65,6 +65,116 @@ namespace RestAPI_Library_Management_System.Controllers
                 Console.WriteLine($"Borrowing transaction with ID {transactionId} has been removed.");
             }
         }
+        [HttpGet("TransactionsByBorrowDate")]
+        public void TransactionsByBorrowDate(DateTime borrowDate)
+        {
+            var transactions = dbContext.BorrowingHistories
+                .Where(history => history.BorrowDate.Date == borrowDate.Date)
+                .ToList();
+
+            if (transactions != null && transactions.Any())
+            {
+               
+                foreach (var transaction in transactions)
+                {
+                    Console.WriteLine($"Book Title: {transaction.book.Title}");
+                    Console.WriteLine($"Borrow Date: {transaction.BorrowDate}");
+                    Console.WriteLine($"Return Date: {transaction.ReturnDate ?? DateTime.MinValue}"); // Use DateTime.MinValue if ReturnDate is null
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+
+                Console.WriteLine($"No transactions found with Borrow Date {borrowDate.Date}.");
+            }
+        }
+
+        [HttpGet("TransactionsByReturnDate")]
+        public void TransactionsByReturnDate(DateTime returnDate)
+        {
+            var transactions = dbContext.BorrowingHistories
+                .Where(history => history.ReturnDate != null && history.ReturnDate.Value.Date == returnDate.Date)
+                .ToList();
+
+            if (transactions != null && transactions.Any())
+            {
+               
+                foreach (var transaction in transactions)
+                {
+                    Console.WriteLine($"Book Title: {transaction.book.Title}");
+                    Console.WriteLine($"Borrow Date: {transaction.BorrowDate}");
+                    Console.WriteLine($"Return Date: {transaction.ReturnDate ?? DateTime.MinValue}"); // Use DateTime.MinValue if ReturnDate is null
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+
+                Console.WriteLine($"No transactions found with Return Date {returnDate.Date}.");
+            }
+        }
+
+        [HttpGet("TransactionsByPatron")]
+        public void TransactionsByPatron(int patronId)
+        {
+            var transactions = dbContext.BorrowingHistories
+                .Include(history => history.patron)
+                .Where(history => history.PatronId == patronId)
+                .ToList();
+
+            if (transactions != null && transactions.Any())
+            {
+               
+                foreach (var transaction in transactions)
+                {
+                    Console.WriteLine($"Book Title: {transaction.book.Title}");
+                    Console.WriteLine($"Borrow Date: {transaction.BorrowDate}");
+                    Console.WriteLine($"Return Date: {transaction.ReturnDate ?? DateTime.MinValue}"); // Use DateTime.MinValue if ReturnDate is null
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+               
+                Console.WriteLine($"No transactions found for Patron with ID {patronId}.");
+            }
+        }
+
+        [HttpGet("TransactionsByBook")]
+        public void TransactionsByBook(int bookId)
+        {
+            var transactions = dbContext.BorrowingHistories
+                .Include(history => history.book)
+                .Where(history => history.BookId == bookId)
+                .ToList();
+
+            if (transactions != null && transactions.Any())
+            {
+               
+                foreach (var transaction in transactions)
+                {
+                    Console.WriteLine($"Book Title: {transaction.book.Title}");
+                    Console.WriteLine($"Borrow Date: {transaction.BorrowDate}");
+                    Console.WriteLine($"Return Date: {transaction.ReturnDate ?? DateTime.MinValue}"); // Use DateTime.MinValue if ReturnDate is null
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+            
+               Console.WriteLine($"No transactions found for Book with ID {bookId}.");
+            }
+        }
+
+        [HttpGet("BorrowCountForBook")]
+        public void BorrowCountForBook(int bookId)
+        {
+            var borrowCount = dbContext.BorrowingHistories
+                .Count(history => history.BookId == bookId);
+
+           Console.WriteLine($"Borrow count for Book with ID {bookId}: {borrowCount}");
+        }
 
     }
 }
